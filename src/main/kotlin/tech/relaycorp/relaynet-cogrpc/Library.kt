cdcfc6621459962dabad38ac354416791a4a0ff2
Gus Narea <gnarea@users.noreply.github.com>
2020-03-16 12:24:18 +0000

    Import files from JVM library

---
+++ src/main/kotlin/tech/relaycorp/relaynet-cogrpc/Library.kt
@@ -0,0 +1,7 @@
+package tech.relaycorp.relaynet-cogrpc
+
+class Library {
+    fun someLibraryMethod(): Boolean {
+        return true
+    }
+}
---
+++ src/test/kotlin/tech/relaycorp/relaynet-cogrpc/LibraryTest.kt
@@ -0,0 +1,11 @@
+package tech.relaycorp.relaynet-cogrpc
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class LibraryTest {
+    @Test fun testSomeLibraryMethod() {
+        val classUnderTest = Library()
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'")
+    }
+}
