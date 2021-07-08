0566ed15cf1a7639558b1fd4f19608d8c29e24ec
Gus Narea <gnarea@users.noreply.github.com>
2021-07-08 23:12:56 +0100

    chore: Move Gradle Enterprise config to settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "cogrpc"
+
+plugins {
+    id("com.gradle.enterprise") version "3.6.3"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
